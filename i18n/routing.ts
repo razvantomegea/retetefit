a6370d1ff244664bf5b61d0dcc1f0e01ba2c3e97
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-11-03 09:11:38 +0200

    Integrate next-intl for internationalization support and update configuration. Remove default page and layout components for a cleaner structure.

---
+++ i18n/routing.ts
@@ -0,0 +1,28 @@
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+import { locales, defaultLocale } from './config';
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales,
+
+  // Used when no locale matches
+  defaultLocale,
+
+  // The `pathnames` object holds pairs of internal and
+  // external pathnames. Based on the locale, the
+  // external pathnames are rewritten to the shared,
+  // internal format.
+  //
+  // Example: `/de/team` → `/en/team`
+  // Example: `/en/team` → `/en/team`
+  pathnames: {
+    '/': '/'
+  }
+});
+
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
+
